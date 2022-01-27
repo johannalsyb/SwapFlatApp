46b7c7eb2dbc61ccd7c1a21ced03a7dea9be9317
Johann <johann.alerte@gmail.com>
2022-01-27 11:05:39 +0700

    card component

--- screens/ListingScreen.js
+++ screens/ListingScreen.js
@@ -1,29 +1,49 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 import Card from "../components/Card";
 import colors from "../config/colors";
+
+const cardList = [
+  {
+    id: 1,
+    title: "Product 1",
+    description: "Le premier produit",
+    image: require("../assets/elephant.jpg"),
+  },
+  {
+    id: 2,
+    title: "Product 2",
+    description: "Le deuxieme produit",
+    image: require("../assets/elephant.jpg"),
+  },
+];
 
 function ListingScreen(props) {
   return (
     <View style={styles.container}>
-      <Card image={require("../assets/elephant.jpg")}
-      title="Product"
-      description="kdjekjd" />
-      <Card image={require("../assets/elephant.jpg")}
-      title="Product"
-      description="kdjekjd" />
-     
+      <FlatList
+        data={cardList}
+        keyExtractor={(list) => list.id.toString()}
+        renderItem={({ item }) => (
+          <Card
+            title={item.title}
+            description={item.description}
+            image={item.image}
+          />
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-      width: "100%",
-      height: "100%",
-      paddingHorizontal: 20,
-      paddingVertical: 20,
-      backgroundColor: colors.back,
+    width: "100%",
+    height: "100%",
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    backgroundColor: colors.back,
   },
 });
 export default ListingScreen;
